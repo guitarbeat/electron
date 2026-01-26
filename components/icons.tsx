1cf6aefa90760863df8e401489b3196eaf548d61
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2026-01-26 00:14:57 -0600

    123

--- components/icons.tsx
+++ components/icons.tsx
@@ -188,12 +188,8 @@ export const MagicWandIcon: React.FC<{ className?: string; style?: React.CSSProp
   const defaultStyle: React.CSSProperties = { width: '1.25rem', height: '1.25rem', ...style };
   return (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} style={defaultStyle} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
-       {/* Better Wand Icon */}
-       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" style={{display: 'none'}} /> 
-       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 4v2m-2-2h2m2 0H15m-1 11l-4 4m0 0l-4-4m4 4V10" style={{display: 'none'}} />
-       
-       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
+       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 21l10-10m5-5l-2.5 2.5" />
+       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 3.5l1.5 1.5M19 7.5L21 9M21 3.5l-2 2M15 9.5l1.5-1.5" />
     </svg>
   );
 };
