e2099c67b14f11446c10bf8e1c3724522daaa9ad
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2026-03-31 00:36:34 +0000

    refactor: remove unused colorUtils export and usePlacesAutocomplete hook
    Manually applies changes from two conflicting branches:
    - fix/remove-unused-color-utils: colorUtils in styling.ts was unused
    - remove-unused-useplacesautocomplete: hook moved to places/ but not imported anywhere

--- src/hooks/places/index.ts
+++ src/hooks/places/index.ts
@@ -1,4 +1,3 @@
 // Places hooks module
 export { usePlaces } from './usePlaces';
 export { usePlaceSuggestions } from './usePlaceSuggestions';
-export { usePlacesAutocomplete } from './usePlacesAutocomplete';
