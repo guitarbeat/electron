8f6208200942f36f57c6043421a714a69b6d4917
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2026-04-10 17:16:53 -0500

    hopefully fixes and makes console easier to debug

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { applyFetchResponseHeaders } from './api/_lib/nodeResponse.ts';
+import { applyFetchResponseHeaders } from './api/_lib/nodeBridge.ts';
 
 const resolveFromRoot = (subpath: string): string => path.resolve(__dirname, subpath);
 
