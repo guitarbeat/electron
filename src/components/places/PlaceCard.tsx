3976c14c99f2bac9f97cadf499a3ebc45d4362b0
guitarbeat <83775388+guitarbeat@users.noreply.github.com>
2026-05-22 19:36:51 +0000

    perf: optimize mergeMovieAutocompleteResults allocations
    Co-authored-by: Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>

--- src/components/places/PlaceCard.tsx
+++ src/components/places/PlaceCard.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import React, { useState } from 'react';
 import { CheckIcon, EditIcon, TrashIcon } from '@/common/Icons';
 import MediaCard from '@/ui/MediaCard';
