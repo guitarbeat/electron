ddb1e497a18dd9bd8a7279e7e6b4bab2feaa46fe
guitarbeat <83775388+guitarbeat@users.noreply.github.com>
2026-06-10 10:02:20 +0000

    Fix CI errors in unused vars and exhaustiveness
    Removed unused 'React' import from MagicToggle.tsx.
    Added missing dependency 'toggle' to useEffects in ProfileMenu.tsx and removed unused MouseEvent.
    Changed the 'v' arg in storageClient.test.ts to actually be used (v !== undefined).
    
    Co-authored-by: Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>

--- src/components/ui/MagicToggle.tsx
+++ src/components/ui/MagicToggle.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import './MagicToggle.css';
 
 export interface MagicToggleOption<T extends string> {
